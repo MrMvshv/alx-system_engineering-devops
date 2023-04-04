0b24140126c64590f47487fc2a7bdc0c746d655d
MrMvshv <machariajosphat001@gmail.com>
2023-04-04 09:29:12 +0000

    server puppet 404

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,55 +1,16 @@
-# Install and configure Nginx with a custom response header
-
-# update software packages list
-exec { 'update packages':
-  command => 'apt-get update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# install nginx
-package { 'nginx':
-  ensure  => 'installed',
-  name    => 'nginx'
-  require => Exec['update packages'],
-}
-
-# Create the hello world page
-file { '/var/www/html/index.html':
-  ensure  => present,
-  path    => '/var/www/html/index.html',
-  content => 'Hello World!\n'
-  require => Package['nginx']
-}
-
-# Create redirection for website
-file_line { 'redirect':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-# Configure Nginx to use the custom 404 page
-file_line { '404_page':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server_name _;',
-  line    => "error_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\tinternal;\n\t}"
-  require => Package['nginx'],
-}
-
-# Add custom header configuration to Nginx
-file_line { 'Header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure => running,
-  require => Package['nginx'],
+# configure nginx with a custom HTTP header response
+exec { 'command':
+  command  => 'sudo apt-get -y update;
+    sudo apt-get -y install nginx;
+    sudo ufw allow "Nginx HTTP";
+    sudo mkdir -p /var/www/html;
+    sudo chmod -R 755 /var/www;
+    echo "Hello World!" | sudo tee /var/www/html/index.html;
+    echo "Ceci n'est pas une page" | sudo tee /var/www/html/404.html;
+    sudo sed -i '/server_name _;/a \        location /redirect_me {\n       return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n        }' /etc/nginx/sites-available/default;
+    sudo sed -i '/server_name _;/a \        error_page 404 /custom_404.html;\n        location = /custom_404.html {\n                internal;\n        }' /etc/nginx/sites-available/default;
+    HOST=$(hostname);
+    sudo sed -i "/^http {/a \    add_header X-Served-By $HOST;" /etc/nginx/nginx.conf;
+    sudo service nginx restart',
+  provider => shell,
 }
