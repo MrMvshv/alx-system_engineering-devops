6178a462028e488001b60988434897bae25b2ce6
MrMvshv <machariajosphat001@gmail.com>
2023-04-24 17:30:55 +0000

    Documentation bug bounty 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """fetches information from JSONplaceholder API and exports to CSV"""
-import csv
+
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+if __name__ == '__main__':
+    link = 'https://jsonplaceholder.typicode.com/'
+    data = requests.get(link + 'users/{}'.format(argv[1])).json()
+
+    u_tasks = requests.get(link + 'todos', params={'userId': argv[1]}).json()
+
+    with open(f"{argv[1]}.csv", 'w') as csv_file:
+        for t in u_tasks:
+            u_id = t.get('userId')
+            u_nm = data.get('username')
+            u_ct = t.get('completed')
+            u_tt = t.get('title')
+
+            txt = '"{}","{}","{}","{}"\n'.format(u_id, u_nm, u_ct, u_tt)
+            csv_file.write(txt)
