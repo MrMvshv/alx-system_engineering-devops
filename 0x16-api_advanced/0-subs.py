6346ace7dfc7d0cd1a202502afc51d2fa9a5f067
MrMvshv <machariajosphat001@gmail.com>
2023-05-09 21:39:10 +0000

    API advanced documentation 0.0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,8 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-
+    """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
 
