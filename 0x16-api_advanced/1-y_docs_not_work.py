2f18fd7c795387574ea54666f8092edafb0d1d9f
MrMvshv <machariajosphat001@gmail.com>
2023-05-09 21:56:16 +0000

    API advanced documentation 1.3

--- 0x16-api_advanced/1-y_docs_not_work.py
+++ 0x16-api_advanced/1-y_docs_not_work.py
@@ -8,13 +8,15 @@ def top_ten(subreddit):
     """get top ten hot topics"""
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": 'My agent'}
+    headers = {"User-Agent": "My agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        posts = response.json()
+        data = response.json()
+        posts = data["data"]["children"]
 
-        for post in posts['data']['children']:
-            print(post['data']['title'])
+        for post in posts:
+            title = post["data"]["title"]
+            print(title)
     else:
         print(None)
