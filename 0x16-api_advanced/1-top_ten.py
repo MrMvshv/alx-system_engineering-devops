212fc8ec66504299b65f20bbfffa090e8439352b
MrMvshv <machariajosphat001@gmail.com>
2023-05-09 21:58:51 +0000

    API advanced documentation 1.4

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 def top_ten(subreddit):
     """get top ten hot topics"""
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": 'My agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
